6965e011d48d7474c1805cd63d72477c19e3ba6c
Sectumsoft <sectumsoft@gmail.com>
2025-12-01 14:50:10 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/green_gaze/", // 👈 EXACTLY your repo name
+  base: "/Rinoof-Cattle-Food/", // 👈 EXACTLY your repo name
   plugins: [react()],
   resolve: {
     alias: {
